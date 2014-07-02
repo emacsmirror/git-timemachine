6274adc5e9a5126bdf45d3d6e152cebed77d1811
Peter Stiernström <peter.stiernstrom@bukowskis.com>
2014-07-02 09:10:49 +0200

    Adds some package headers.

--- git-timemachine.el
+++ git-timemachine.el
@@ -1,4 +1,28 @@
+;;; git-timemachine.el --- Walk through git revisions of a file
+
 ;;; -*- lexical-binding: t -*-
+
+;; Copyright (C) 2014 Peter Stiernström
+
+;; Author: Peter Stiernström <peter@stiernstrom.se>
+;; Version: 0.1
+;; URL: https://github.com/pidu/git-timemachine
+;; Keywords: git
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'subr-x)
 (require 'cl-lib)
@@ -54,6 +78,7 @@
    (insert this-revision)
    (kill-region (point-min) (point-max)))))
 
+;;;###autoload
 (defun git-timemachine-mode ()
  (interactive)
  (let* ((git-directory (concat (string-trim-right (shell-command-to-string "git rev-parse --show-toplevel")) "/"))
@@ -73,3 +98,5 @@
    (switch-to-buffer timemachine-buffer))))
 
 (provide 'git-timemachine)
+
+;; git-timemachine.el ends here
