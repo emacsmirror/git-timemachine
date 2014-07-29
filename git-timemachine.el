cf89165bbe8cbac66d61f20238fa62101a7cf61e
Peter <peter@stiernstrom.se>
2014-07-29 19:19:39 +0200

    Make previous PR pass checkdoc and bumped revision.

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 1.4
+;; Version: 1.5
 ;; URL: https://github.com/pidu/git-timemachine
 ;; Package-Requires: ((cl-lib "0.5"))
 ;; Keywords: git
@@ -98,11 +98,12 @@
  :group 'git-timemachine)
 
 (defun git-timemachine-validate (file)
+ "Validate that there is a FILE and that it belongs to a git repository. Call with the value of 'buffer-file-name."
  (unless file
-  (error "This buffer is not related to file."))
+  (error "This buffer is not related to file"))
  (with-temp-buffer
   (unless (zerop (call-process "git" nil nil nil "ls-files" "--error-unmatch" file))
-   (error "This file is not git tracked."))))
+   (error "This file is not git tracked"))))
 
 ;;;###autoload
 (defun git-timemachine ()
