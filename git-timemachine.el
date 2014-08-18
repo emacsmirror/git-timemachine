84874250f47b980bb60668e4e8f5c5d6867a96e3
Peter Stiernström <peter.stiernstrom@bukowskis.com>
2014-08-18 09:57:10 +0200

    Bumped version to celebrate tramp support.

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 1.5
+;; Version: 1.6
 ;; URL: https://github.com/pidu/git-timemachine
 ;; Package-Requires: ((cl-lib "0.5"))
 ;; Keywords: git
