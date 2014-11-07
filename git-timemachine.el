f9eac8aebcc492e9a9a2dbfe5a0e68ea5005b136
Peter Stiernström <peter@mondieu.infinet>
2014-11-07 22:04:45 +0100

    Drop dependency on cl-lib.

--- git-timemachine.el
+++ git-timemachine.el
@@ -5,7 +5,6 @@
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
 ;; Version: 1.8
 ;; URL: https://github.com/pidu/git-timemachine
-;; Package-Requires: ((cl-lib "0.5"))
 ;; Keywords: git
 
 ;; This file is not part of GNU Emacs
@@ -30,7 +29,6 @@
 
 ;;; Code:
 
-(require 'cl-lib)
 (require 'vc-git)
 
 (defcustom git-timemachine-abbreviation-length 12
