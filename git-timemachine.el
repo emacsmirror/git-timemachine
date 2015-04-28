4cf089897f128f34438dcdcf1819a47bb46d7a45
Peter Stiernström <peter.stiernstrom@bukowskis.com>
2015-04-28 08:16:14 +0200

    Bumped version.

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 2.3
+;; Version: 2.4
 ;; URL: https://github.com/pidu/git-timemachine
 ;; Keywords: git
 
