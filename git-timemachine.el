965dd451a5df1993887f3cb76ec6e07a6c955c5a
Peter Stiernström <peter@stiernstrom.se>
2015-05-13 23:57:44 +0200

    Bumped revision

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 2.5
+;; Version: 2.6
 ;; URL: https://github.com/pidu/git-timemachine
 ;; Keywords: git
 
