6e2754b02afc059d431de71724a7186f051d965a
Peter Stiernström <peter.stiernstrom@bukowskis.com>
2016-11-15 23:20:03 +0100

    Bump revision

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 3.1
+;; Version: 4.0
 ;; URL: https://github.com/pidu/git-timemachine
 ;; Keywords: git
 ;; Package-Requires: ((emacs "24.3"))
