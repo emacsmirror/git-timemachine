6467b03d9826275e0dd2534ac6c32ed08d733e1f
Dan Harms <dan.harms@xrtrading.com>
2017-02-21 16:38:46 -0600

    Issue 58 - commit details illegible with light themes

--- git-timemachine.el
+++ git-timemachine.el
@@ -48,14 +48,20 @@ will be shown in the minibuffer while navigating commits."
  :group 'git-timemachine)
 
 (defface git-timemachine-minibuffer-detail-face
- '((t (:foreground "yellow")))
- "How to display the minibuffer detail"
- :group 'git-timemachine)
+  '((((class color) (background dark))
+     :foreground "yellow")
+    (((class color) (background light))
+     :foreground "yellow4"))
+  "How to display the minibuffer detail"
+  :group 'git-timemachine)
 
 (defface git-timemachine-minibuffer-author-face
- '((t (:foreground "orange")))
- "How to display the author in minibuffer"
- :group 'git-timemachine)
+  '((((class color) (background dark))
+     :foreground "orange")
+    (((class color) (background light))
+     :foreground "DarkOrange4"))
+  "How to display the author in minibuffer"
+  :group 'git-timemachine)
 
 (defcustom git-timemachine-minibuffer-detail
  'subject
