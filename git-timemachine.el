ac933e5cd29583c131401f3bd991d98129c316df
pidu <pidu@noreply.codeberg.org>
2023-06-30 12:14:11 +0000

    bump version
    4.13

--- git-timemachine.el
+++ git-timemachine.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2014 Peter Stiernström
 
 ;; Author: Peter Stiernström <peter@stiernstrom.se>
-;; Version: 4.12
+;; Version: 4.13
 ;; URL: https://gitlab.com/pidu/git-timemachine
 ;; Keywords: vc
 ;; Package-Requires: ((emacs "24.3") (transient "0.1.0"))
